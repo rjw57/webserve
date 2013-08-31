927c7850fa65b468ac1f790f8056cc21b4f56369
Rich Wareham <rjw57@cam.ac.uk>
2013-08-31 14:56:50 +0100

    add configurable bind port and address

--- webserve.go
+++ webserve.go
@@ -1,9 +1,15 @@
 package main
 
-import "net/http"
+import "flag"
+import "fmt"
 import "log"
+import "net/http"
 
 func main() {
+	var specStringPtr = flag.String("addr", ":8080", "[address][:port] to serve from")
+	flag.Parse()
+
 	http.Handle("/", http.FileServer(http.Dir(".")))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println(fmt.Sprintf("Serving current directory on %s", *specStringPtr))
+	log.Fatal(http.ListenAndServe(*specStringPtr, nil))
 }
